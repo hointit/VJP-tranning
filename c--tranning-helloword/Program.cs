f6b1da13e4573f0822bc647199b456b03ddb4848
Tien Hoi Nguyen <hoint@techvn.mti.co.jp>
2020-01-07 19:53:08 +0700

    Create project

---
+++ c--tranning-helloword/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace c__tranning_helloword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
