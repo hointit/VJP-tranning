69fff6e1e99c2cccf74c95ac5f58169c403834b5
hoint <hointit@gmail.com>
2020-02-17 18:56:32 +0700

    merge conflict hoint

--- c--tranning-helloword/Program.cs
+++ c--tranning-helloword/Program.cs
@@ -6,6 +6,8 @@ namespace c__tranning_helloword
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello Hoint!");
+            Console.WriteLine();
             Console.WriteLine("Hello World!");
             Console.ReadLine();
         }
