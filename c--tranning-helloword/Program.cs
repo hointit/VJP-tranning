bfd2fcebccc8757e35471fd5352ec0a9e4d0fb25
666 <666@DESKTOP-P75JFV6>
2020-02-17 19:43:23 +0700

    Tai hello

--- c--tranning-helloword/Program.cs
+++ c--tranning-helloword/Program.cs
@@ -6,7 +6,7 @@ namespace c__tranning_helloword
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Tai!");
+            Console.WriteLine("Tai hello!");
         }
     }
 }
