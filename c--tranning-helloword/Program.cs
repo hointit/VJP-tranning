b3b1a66a6f723e3721064abb9f7daa4a6adaf510
tphong3099 <tphong3099@gmail.com>
2020-02-17 19:05:58 +0700

    phong

--- c--tranning-helloword/Program.cs
+++ c--tranning-helloword/Program.cs
@@ -6,7 +6,7 @@ namespace c__tranning_helloword
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Phong!");
         }
     }
 }
